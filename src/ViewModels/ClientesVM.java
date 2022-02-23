da73446eccdd8c749bea89d6f88c781f1ed20f94
frodriguez <frodriguez@NB-016.tocsa.com.py>
2022-02-23 17:32:27 -0300

    Se agregaron controles JTable y clase de búsqueda en BBDD

--- src/ViewModels/ClientesVM.java
+++ src/ViewModels/ClientesVM.java
@@ -6,12 +6,14 @@ package ViewModels;
 
 import Conexion.Consult;
 import Library.Calendario;
+import Library.RenderCheckBox;
 import Models.TCliente;
 import java.awt.Color;
 import java.util.*;
 import javax.swing.*;
 import java.sql.*;
 import java.util.stream.Collectors;
+import javax.swing.table.DefaultTableModel;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ColumnListHandler;
 
@@ -24,12 +26,19 @@ public class ClientesVM extends Consult {
     private String _accion = "insert";
     private final ArrayList<JLabel> _label;
     private final ArrayList<JTextField> _textField;
-    private JCheckBox _checkBoxCredito;
+    private final JCheckBox _checkBoxCredito;
+    private final JTable _tableCliente;
+    private DefaultTableModel modelo1;
+    private final int _idCliente = 0;
+    private final int _reg_por_pagina = 10;
+    private final int _num_paginas = 1;
 
     public ClientesVM(Object[] objects, ArrayList<JLabel> label, ArrayList<JTextField> textField) {
         _label = label;
         _textField = textField;
         _checkBoxCredito = (JCheckBox) objects[0];
+        _tableCliente = (JTable) objects[1];
+        restablecer();
     }
 
     public void registrarCliente() {
@@ -112,10 +121,65 @@ public class ClientesVM extends Consult {
                 cliente.get(cliente.size() - 1).getId(),};
             qr.insert(getConn(), sqlReport, new ColumnListHandler(), dataReport);
             getConn().commit();
+            restablecer();
         } catch (Exception ex) {
             getConn().rollback();
             JOptionPane.showMessageDialog(null, ex);
         }
     }
 
+    public void SearchClientes(String campo) {
+        List<TCliente> clienteFilter;
+        String[] titulos = {"ID", "Cédula", "Nombre", "Apellido", "Email", "Dirección", "Teléfono", "Crédito"};
+        modelo1 = new DefaultTableModel(null, titulos);
+        int inicio = (_num_paginas - 1) * _reg_por_pagina;
+        if (campo.equals("")) {
+            clienteFilter = clientes().stream().skip(inicio).limit(_reg_por_pagina).collect(Collectors.toList());
+        } else {
+            clienteFilter = clientes().stream()
+                    .filter(C -> C.getNumDoc().startsWith(campo) || C.getNombre().startsWith(campo) || C.getApellido().startsWith(campo))
+                    .skip(inicio).limit(_reg_por_pagina).collect(Collectors.toList());
+        }
+        if (!clienteFilter.isEmpty()) {
+            clienteFilter.forEach(item -> {
+                Object[] registros = {
+                    item.getId(),
+                    item.getNumDoc(),
+                    item.getNombre(),
+                    item.getApellido(),
+                    item.getEmail(),
+                    item.getDireccion(),
+                    item.getTelefono(),
+                    item.isCredito()
+                };
+                modelo1.addRow(registros);
+            });
+        }
+        _tableCliente.setModel(modelo1);
+        _tableCliente.setRowHeight(20);
+        _tableCliente.getColumnModel().getColumn(0).setMaxWidth(0);
+        _tableCliente.getColumnModel().getColumn(0).setMinWidth(0);
+        _tableCliente.getColumnModel().getColumn(0).setPreferredWidth(0);
+        _tableCliente.getColumnModel().getColumn(7).setCellRenderer(new RenderCheckBox());
+    }
+
+    public final void restablecer() {
+        _accion = "insert";
+        _textField.get(0).setText("");
+        _textField.get(1).setText("");
+        _textField.get(2).setText("");
+        _textField.get(3).setText("");
+        _textField.get(4).setText("");
+        _textField.get(5).setText("");
+        _checkBoxCredito.setSelected(false);
+
+        _label.get(0).setForeground(new Color(0, 0, 0));
+        _label.get(1).setForeground(new Color(0, 0, 0));
+        _label.get(2).setForeground(new Color(0, 0, 0));
+        _label.get(3).setForeground(new Color(0, 0, 0));
+        _label.get(4).setForeground(new Color(0, 0, 0));
+        _label.get(5).setForeground(new Color(0, 0, 0));
+
+        SearchClientes("");
+    }
 }
